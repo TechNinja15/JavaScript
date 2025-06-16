4d584f6d1c707ce2e62f3d518c1812ee944063f5
TechNinja15 <avneeshjha1506@gmail.com>
2025-06-16 07:23:06 +0000

    Datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using node js, not browser
+
+console.log(3+3); 
+console.log("AKJ");
+
+
+let name = "AKJ";   // string dataType ("")
+let age= 18;        // number
+let isLogggedIn = false  //boolean true/false
+
+console.log(typeof undefined);
+
