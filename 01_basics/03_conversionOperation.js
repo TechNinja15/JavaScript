930cb730c4c546b61c3ce9ace4c37bc610093535
TechNinja15 <avneeshjha1506@gmail.com>
2025-06-16 07:48:09 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,39 @@
+let score = "33abc";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// OutPut
+/* @TechNinja15 ➜ /workspaces/JavaScript (main) $ node 01_basics/03_conversionOperation.js
+string
+string
+number
+NaN
+*/
+
+let isLoggedIn = "";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//OutPut
+/*
+
+0 => false
+1 => true
+""/null/undefined => false
+"AKJ" => true
+
+*/
+
+
+let someNumber = 18;
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
