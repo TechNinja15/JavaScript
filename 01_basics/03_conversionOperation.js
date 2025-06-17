6f56fd35ba6b8614bba70e98d55842f540657f54
TechNinja15 <avneeshjha1506@gmail.com>
2025-06-17 06:34:11 +0000

    conversion and operations on js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -34,6 +34,16 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 18;
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+let num1, num2, num3
+
+num1 = num2= num3 = 2+2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
 
