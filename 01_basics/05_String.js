24adadba0df010a6ec308dbb1b8130c89c5ec52b
TechNinja15 <avneeshjha1506@gmail.com>
2025-06-19 05:52:23 +0000

    all about Strings

---
+++ 01_basics/05_String.js
@@ -0,0 +1,30 @@
+const name = "AKJ"
+const repoCount = 50
+
+//console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+const gameName = new String('Avneesh-KJ-com')
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(7));
+// // console.log(gameName.indexOf('-'))
+
+// const newString = gameName.substring(10 ,4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8 ,4)
+// console.log(anotherString);
+
+
+
+const newStringOne = "    AKJ   "
+console.log(newStringOne.trim());
+
+const url = "https://akj.com/akj%20jha"
+
+console.log((url.replace('%20', '-')));
+console.log(url.includes('om'));
+
+console.log(gameName.split('-'));
