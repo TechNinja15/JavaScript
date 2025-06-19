c2ec3aa4d8e3afa510da45296108e5e6b7645138
TechNinja15 <avneeshjha1506@gmail.com>
2025-06-19 06:47:08 +0000

    Numbers and Math Functions in JS

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,35 @@
+const score = 400;
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.8966;
+
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-In'));
+
+// ++++++++++++++++++++++++ Math ++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-5));
+// // console.log(Math.abs(5));
+// console.log(Math.round(4.7)); //Takes the Nearest value
+// console.log(Math.ceil(4.01)); //Takes the Upper value
+// console.log(Math.floor(4.99)); //Takes the lowest value
+// console.log(Math.min(2,4,3,6,3)); //Takes the minimum value
+// console.log(Math.max(2,4,3,6,3)); //Takes the Maximum value
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+min =10;
+max=20;
+
+console.log(Math.floor(Math.random()*(max-min + 1))+min); 
