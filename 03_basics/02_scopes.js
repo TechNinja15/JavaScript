e73594556d74e8a17bc1f572ff3e86279d024ba2
TechNinja15 <avneeshjha1506@gmail.com>
2025-06-22 10:54:26 +0000

    Scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -8,11 +8,52 @@ let a =300
 if (true) {
     let a = 10
     const b = 20
-    console.log("Inner :", a); // 10
+    // console.log("Inner :", a); // 10
 }
-
+// console.log("Outer :", a); // 300
 
 
 // console.log(a);
 // console.log(b);
-console.log(a);
+
+
+function one(){
+    const username = "Avneesh";
+    function two(){
+        const website = "Youtube";
+        console.log(username); // Accessing variable from outer function        
+    }
+    // console.log(website); // This will throw an error because website is not defined in this scope
+
+    two(); // Calling the inner function to access the variable from outer function
+    
+}
+// one(); // Calling the outer function to access the variable from outer function
+
+
+if (true) {
+    const username = "Avneesh";
+    if (username === "Avneesh") {
+        const website = "Youtube";
+        // console.log(username+ website); // Accessing variable from outer scope
+    }
+    // console.log(website); // This will throw an error because website is not defined in this scope
+
+} 
+
+// console.log(username); // This will throw an error because username is not defined in this scope
+
+
+// ======================== Intresting Example ========================
+
+
+function addOne(num){
+    return num + 1;
+}
+console.log(addOne(5)); // This will return 6
+
+
+const addTwo = function(num){
+    return num + 2;
+}
+addTwo(5); // This will return 7
