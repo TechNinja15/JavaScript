8c5fb353fdca59a6477dfea7bbe056063cebfc08
TechNinja15 <avneeshjha1506@gmail.com>
2025-06-22 16:39:26 +0000

    if, else if , else Statement

---
+++ 04_control_flow/one.js
@@ -0,0 +1,53 @@
+// if Statement
+// const isUserloggedIn = true;
+
+// if (2!="2") {
+//     console.log("true");
+// }
+
+// <,>,<=,>=,==,!= 
+
+// let temperature = 40;
+
+// if ( temperature === 40 ){
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }
+
+// const score = 200;
+// if (score >= 100) {
+//     const power = "fly";
+//     console.log(`User Power: ${power}`);
+// }
+
+
+const balance = 1000;
+// if (balance > 500) console.log("test"); // single line if statement
+
+// if (balance < 500) {
+//     console.log("Balance is less than 500");    
+// }else if (balance < 750) {
+//     console.log("Balance is less than 750");
+// }else if (balance <900) {
+//     console.log("Balance is less than 900");
+// }else{
+//     console.log("Balance is less than 1200");
+// }
+
+
+const userLoggedIn = true;
+const debitCard = true;
+const loggedInFromGoogle = false;
+const loggedInFromEmail = true;
+
+// if (userLoggedIn && debitCard) { // Logical AND operator (&&) : Checks if both conditions are true
+//     console.log("User can make a purchase");     
+// }
+// NOTE : If any of the conditions is false, the code inside the if block will not execute.
+
+// if (loggedInFromGoogle || loggedInFromEmail) { // Logical OR operator (||) : Checks if at least one condition is true
+//     console.log("User can login");  
+// }
+// NOTE : If any of the conditions is true, the code inside the if block will execute.
+
