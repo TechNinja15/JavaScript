8bf1def0c7c9d7005b9cc3f613105c270cdf6a83
TechNinja15 <avneeshjha1506@gmail.com>
2025-06-23 02:58:41 +0000

    All truthy values and false values and ternary operators

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -1,10 +1,10 @@
-const userEmail = []
+// const userEmail = []
 
-if (userEmail) {
-    console.log("Got user email");
-}else{
-    console.log("No user email");
-}
+// if (userEmail) {
+//     console.log("Got user email");
+// }else{
+//     console.log("No user email");
+// }
 
 // In JavaScript, all values are considered "truthy" except for a few specific values.
 // The following values are considered "falsy":
@@ -23,3 +23,24 @@ if (userEmail) {
 // 3. [] (empty array) is truthy
 // 4. {} (empty object) is truthy
 // 5. function(){} (empty function) is truthy
+
+
+// Nullish Coalescing Operator (??) : null undefined
+// The nullish coalescing operator (??) is used to provide a default value when dealing with null or undefined values.
+
+let val1;
+// val1 = 5 ?? 10; // If val1 is null or undefined, it will be set to 10
+// val1 = null ?? 10; // If val1 is null, it will be set to 10
+// val1 = undefined ?? 15; // If val1 is undefined, it will be set to 15
+// val1 = null ?? 5 ?? 10; // If val1 is null, it will be set to 5, otherwise it will be set to 10
+
+// console.log(val1); // Output: 5
+
+
+
+// Ternary Operator (?:) : condition ? expr1 : expr2
+
+const iceTeaPrice = 100;
+iceTeaPrice >= 80 ? console.log("Ice tea is less 80") : console.log("Ice tea is more than 80");
+// The ternary operator is a shorthand way to write an if-else statement. It takes three operands: a condition, an expression to evaluate if the condition is true, and an expression to evaluate if the condition is false.
+// It is often used for simple conditional assignments or to return values based on a condition.
