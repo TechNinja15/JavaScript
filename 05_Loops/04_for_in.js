8b264de86d38dd879d5be309d5d5ba467772b192
TechNinja15 <avneeshjha1506@gmail.com>
2025-06-23 05:25:47 +0000

    For in loops (Mostly used in Objects)

---
+++ 05_Loops/04_for_in.js
@@ -0,0 +1,32 @@
+const myObject ={
+    js: 'JavaScript',
+    py: 'Python',
+    rb: 'Ruby',
+    c: 'C++',
+    swift : 'swift by apple'
+}
+
+// for (const key in myObject) {
+//     console.log(`${key} Short Form is ${myObject[key]}`);
+// }
+
+// The for...in loop is used to iterate over the enumerable properties of an object.
+// It iterates over the keys of the object, allowing you to access both the keys and their corresponding values.
+// Note: The for...in loop is not recommended for iterating over arrays, as it may not guarantee the order of iteration and can include properties from the prototype chain.
+
+const programmingLanguages = ["JavaScript", "Python", "Ruby", "C++", "Swift"];
+
+// for (const key in programmingLanguages) {
+//     console.log(programmingLanguages[key]);   
+// }
+
+// const map = new Map()
+// map.set('IN', "India");
+// map.set('USA', "United States of America");
+// map.set('FR', "France");
+// map.set('IN', "India");
+
+// for (const key in map) {
+//     console.log(key); // This will log the keys of the Map object, but not the values.
+// }
+
