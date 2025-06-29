27441ad5d158551715db85abbf15dfa4d96dd467
TechNinja15 <avneeshjha1506@gmail.com>
2025-06-29 14:54:57 +0000

    Made a background Color changer

---
+++ 07_Projects/01-colorChanger/script.js
@@ -0,0 +1,24 @@
+const randomButton = document.getElementById("randomColor")
+const colorPicker = document.getElementById("colorPicker")
+const colorCode = document.getElementById("colorCode")
+
+
+
+function changeBackgroundColor(color){
+    document.body.style.backgroundColor = color
+    colorCode.textContent = color;
+}
+
+randomButton.addEventListener("click", ()=>{
+    const randomColor = `#${Math.floor(Math.random()*16777215).toString(16)}`
+    changeBackgroundColor(randomColor)
+    console.log(randomColor);
+    
+});
+
+colorPicker.addEventListener('input', (e)=>{
+    // console.log(e.target.value);
+    changeBackgroundColor(e.target.value)
+})
+
+changeBackgroundColor('#b92f1f')
