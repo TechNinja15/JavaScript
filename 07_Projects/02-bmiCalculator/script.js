7692b49a635810f0cb18b94973fcd2ddf31af33d
TechNinja15 <avneeshjha1506@gmail.com>
2025-07-06 09:51:23 +0000

    updated

--- 07_Projects/02-bmiCalculator/script.js
+++ 07_Projects/02-bmiCalculator/script.js
@@ -3,8 +3,8 @@ const height = document.getElementById("height");
 const output = document.getElementById("output");
 
 function calculateBMI() {
-    const weightValue = parseFloat(weight.value);
-    const heightValue = parseFloat(height.value) / 100; // Convert cm to m
+    const weightValue = (weight.value);
+    const heightValue = (height.value) / 100; // Convert cm to m
     const bmi = weightValue / (heightValue * heightValue);
     output.textContent = `${bmi.toFixed(2)}`;
 }
